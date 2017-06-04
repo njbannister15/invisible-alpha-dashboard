b7b370883a901da0e616c4a9e0fb8a0150e20e3d
nbannister <njbannister15@gmail.com>
2017-06-04 14:33:09 -0500

    adding users

--- account-service/src/main/java/com/invisiblealpha/domain/User.java
+++ account-service/src/main/java/com/invisiblealpha/domain/User.java
@@ -1,9 +1,11 @@
 package com.invisiblealpha.domain;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Pattern;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -19,23 +21,30 @@ public class User {
 	
 	public static final PasswordEncoder PASSWORD_ENCODER = new BCryptPasswordEncoder();
 	
-	@Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
-    private Long id;
-	private @JsonIgnore String password;
+	@Id 
+	@GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;	
 	
-	private String email;
     private String firstName;
-    private String lastName;
-	private String[] roles;
+	private String lastName;
+	
+	@Column(unique=true, nullable=false) 
+	@Pattern(regexp = "[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\."
+            + "[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@"
+            + "(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9]"
+            + "(?:[a-z0-9-]*[a-z0-9])?")
+    private String email;
+	
+	@JsonIgnore
+    private String password;
     
-    @SuppressWarnings("unused")
-	private User(){};
+	protected User(){};
     
-    public User(String email, String password, String... roles) {
-		this.email = email;
-		this.setPassword(password);
-		this.roles = roles;
+    public User(String firstName, String lastName, String email, String password) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+    	this.email = email;		
+		this.setPassword(password);		
 	}
    
     public void setPassword(String password) {
--- account-service/src/main/java/com/invisiblealpha/repositories/UserRepository.java
+++ account-service/src/main/java/com/invisiblealpha/repositories/UserRepository.java
@@ -7,10 +7,5 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import com.invisiblealpha.domain.User;
 
 public interface UserRepository extends PagingAndSortingRepository<User, Long> {
-
-	 List<User> findByLastName(String lastName);
-	 List<User> findByFirstName(String firstname);
-	 List<User> findByEmail(String email);
-	 List<User> findById(Long id);
 	 
 }
